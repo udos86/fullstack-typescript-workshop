95d19a704dbff29e46b943bca02d411ce4d99113
Udo Schöfer <developer@udos86.de>
2020-09-01 09:51:15 +0200

    adds Nest form endpoint

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { CreateUserDto } from './user.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Post('/users')
+  create(@Body() body: CreateUserDto) {
+    console.log(body);
+    return `"New user ${body.firstName} ${body.lastName} created"`;
   }
 }
